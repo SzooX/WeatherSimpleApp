aaf0b5e1eed46c8a2f14859e0297ffdbffb05d2d
SzooX <maciekw3000@interia.pl>
2020-07-16 17:13:29 +0200

    API model update

--- WeatherSimpleApp/WeatherSimpleApp/Models/Constants.cs
+++ WeatherSimpleApp/WeatherSimpleApp/Models/Constants.cs
@@ -8,5 +8,6 @@ namespace WeatherSimpleApp.Models
     {
         public const string OpenWeatherMapAPIKey = "a806c3f6c1d40040137bf0ed5e6f5694";
         public const string OpenWeatherMapEndpoint_actual = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapEndpoint_all = "https://api.openweathermap.org/data/2.5/onecall?";
     }
 }
--- WeatherSimpleApp/WeatherSimpleApp/Models/WeatherData.cs
+++ WeatherSimpleApp/WeatherSimpleApp/Models/WeatherData.cs
@@ -16,14 +16,41 @@ namespace WeatherSimpleApp.Models
             AS = new ApiService();
         }
         /// <summary>
-        /// Pass city name to receive actual data about weather that city
+        /// Returns actual data about weather at passed city
         /// </summary>
         /// <param name="city">City name</param>
-        /// <returns>Informations about actual weather in passed city</returns>
-        async public Task<dynamic> GetActualWeather(string city)
+        /// <returns>Returns actual data about weather at passed city</returns>
+        async public Task<dynamic> GetWeather(string city)
         {
             dynamic _data = null;
             _data = await AS.GetWeatherDataAsync(GenerateRequestUri(Constants.OpenWeatherMapEndpoint_actual, city));
+            return _data;
+        }
+        /// <summary>
+        /// Returns weather for specifed range for passed city
+        /// </summary>
+        /// <param name="city">City name</param>
+        /// <param name="range">range can be equal to "actual", "minutely", "hourly" or "daily"</param>
+        /// <returns>Returns weather for specifed range for passed city</returns>
+        async public Task<dynamic> GetWeather(string city , string range)
+        {
+            dynamic _data = null;
+            _data = await AS.GetWeatherDataAsync(GenerateRequestUri(Constants.OpenWeatherMapEndpoint_actual, city));
+            switch (range)
+            {
+                case "actual":
+                    {
+                        //todo
+                        break;
+                    }
+                default:
+                    {
+                        string returns = "Range value is invaild";
+                        return returns;
+                        
+                    }
+            }
+
             return _data;
         }
         string GenerateRequestUri(string endpoint, string Cityname)
@@ -31,6 +58,15 @@ namespace WeatherSimpleApp.Models
             string requestUri = endpoint;
             requestUri += $"?q={Cityname}";
             requestUri += "&units=imperial"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            Console.WriteLine(requestUri);
+            return requestUri;
+        }string GenerateRequestUri(string endpoint, string lat, string lon, string excludings)
+        {
+            string requestUri = endpoint;
+            requestUri += $"lat={lat}";
+            requestUri += $"&lon={lon}";
+            requestUri += $"&exclude={excludings}";
             requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
             Console.WriteLine(requestUri);
             return requestUri;
