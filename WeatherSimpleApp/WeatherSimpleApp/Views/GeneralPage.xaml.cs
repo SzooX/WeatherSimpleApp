179e7d3135ed9c0cd237eb79bb6ba337a6597979
SzooX <maciekw3000@interia.pl>
2020-07-16 16:45:43 +0200

    .

--- WeatherSimpleApp/WeatherSimpleApp/Views/GeneralPage.xaml.cs
+++ WeatherSimpleApp/WeatherSimpleApp/Views/GeneralPage.xaml.cs
@@ -15,22 +15,6 @@ namespace WeatherSimpleApp
         public GeneralPage()
         {
             InitializeComponent();
-            MasterPage.ListView.ItemSelected += ListView_ItemSelected;
-        }
-
-        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            var item = e.SelectedItem as GeneralPageMasterMenuItem;
-            if (item == null)
-                return;
-
-            var page = (Page)Activator.CreateInstance(item.TargetType);
-            page.Title = item.Title;
-
-            Detail = new NavigationPage(page);
-            IsPresented = false;
-
-            MasterPage.ListView.SelectedItem = null;
         }
     }
 }
