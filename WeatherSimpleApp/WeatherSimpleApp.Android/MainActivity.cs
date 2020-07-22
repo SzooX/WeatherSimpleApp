b7ae80527fb8980d19e3f69c0d922fb8559041b1
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2020-07-22 15:27:59 +0200

    Splash screen and color changes

--- WeatherSimpleApp/WeatherSimpleApp.Android/MainActivity.cs
+++ WeatherSimpleApp/WeatherSimpleApp.Android/MainActivity.cs
@@ -6,10 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Graphics;
+using Xamarin.Essentials;
 
 namespace WeatherSimpleApp.Droid
 {
-    [Activity(Label = "WeatherSimpleApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "WeatherSimpleApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -25,13 +27,26 @@ namespace WeatherSimpleApp.Droid
             global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
             
             LoadApplication(new App());
-            
+
+            //Set status bar color
+            var color = Color.ParseColor("#606060");
+            var systemColor = color.ToSystemColor();
+            var systemColor2 = DarkerColor(systemColor);
+            var platformColor = systemColor2.ToPlatformColor();
+            Window.SetStatusBarColor(platformColor);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private System.Drawing.Color DarkerColor(System.Drawing.Color color, float correctionfactory = 50f)
+        {
+            const float hundredpercent = 100f;
+            return System.Drawing.Color.FromArgb((int)(((float)color.R / hundredpercent) * correctionfactory),
+                (int)(((float)color.G / hundredpercent) * correctionfactory), (int)(((float)color.B / hundredpercent) * correctionfactory));
         }
     }
 }
